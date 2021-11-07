013eaa6f4ed16df3afa1e8e92e49917ac2dbc322
kamoto0427 <takahiro.08039@gmail.com>
2021-11-07 20:40:17 +0900

    イベント、カテゴリー一覧表示(詳細レイアウトなし)

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->category = new Category();
+    }
+
     /**
      * カテゴリー一覧画面
      */
     public function index()
     {
-        return view('category.index');
+        // categoriesテーブルのレコードを全件取得
+        $categories = $this->category->allCategoriesData();
+        
+        return view('category.index', compact('categories'));
     }
 }
